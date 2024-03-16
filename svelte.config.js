b0d6dd0d2821377e6c4e683be0fbb683899bc068
katriellucas <katriel.developer@gmail.com>
2024-03-16 20:00:04 -0300

    temp: test deploy

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ edge: true }),
+		adapter: adapter(),
 		alias: {
 			$components: 'src/components/*',
 			$lib: 'src/lib/'
